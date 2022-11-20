51fde948d191d2e69ceef4c9cf019ee8677acfef
BeoLem <102394603+BeoLem@users.noreply.github.com>
2022-11-20 11:21:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
 const timers = [
   {
     id: 1,
-    ms: 0,
     s: 0,
     m: 0,
     stopCounting: false,
   },
   {
     id: 2,
-    ms: 0,
     s: 0,
     m: 0,
     stopCounting: false,
   },
   {
     id: 3,
-    ms: 0,
     s: 0,
     m: 0,
     stopCounting: false,
   },
   {
     id: 4,
-    ms: 0,
     s: 0,
     m: 0,
     stopCounting: false,
   },
   {
     id: 5,
-    ms: 0,
     s: 0,
     m: 0,
     stopCounting: false,
@@ -57,12 +52,21 @@ stopButton(document.querySelector('button.stop[data-id="3"]'));
 stopButton(document.querySelector('button.stop[data-id="4"]'));
 stopButton(document.querySelector('button.stop[data-id="5"]'));
 
+pauseButton(document.querySelector('button.pause[data-id="1"]'));
+pauseButton(document.querySelector('button.pause[data-id="2"]'));
+pauseButton(document.querySelector('button.pause[data-id="3"]'));
+pauseButton(document.querySelector('button.pause[data-id="4"]'));
+pauseButton(document.querySelector('button.pause[data-id="5"]'));
+
 document.querySelector("button.stopAll").addEventListener("click", () => {
   timers.map((v) => {
-    stopp(v, v.id)
-    setInterval(() => {
-        stopp(v, v.id)
-    }, 2)
+    stopp(v, v.id);
+  });
+});
+
+document.querySelector("button.startAll").addEventListener("click", () => {
+  timers.map((v) => {
+    startt(v, v.id);
   });
 });
 
@@ -70,18 +74,17 @@ function startButton(button) {
   const id = button.dataset.id;
   const obj = timers[id - 1];
 
-  button.addEventListener("click", () => startt(obj, id));
+  button.addEventListener("click", () => {
+    timers[id - 1].stopCounting = false;
+    startt(obj, id);
+  });
 }
 
 function startt(obj, id) {
   obj.stopCounting = false;
   const interval = setInterval(() => {
-    if (obj.stopCounting == true) clearInterval(interval);
-    obj.ms++;
-    if (obj.ms == 100) {
-      obj.s++;
-      obj.ms = 0;
-    }
+    if (obj.stopCounting) return clearInterval(interval);
+    obj.s++;
     if (obj.s == 60) {
       obj.m++;
       obj.s = 0;
@@ -89,12 +92,11 @@ function startt(obj, id) {
 
     const minutes = obj.m < 10 ? `0${obj.m}` : obj.m;
     const seconds = obj.s < 10 ? `0${obj.s}` : obj.s;
-    const ms = obj.ms < 10 ? `0${obj.ms}` : obj.ms;
 
     timerList[
       id - 1
-    ].children[0].innerHTML = `<span>${id}. ${minutes}:${seconds}:${ms}</span>`;
-  }, 1);
+    ].children[0].innerText = `${id}. ${minutes}:${seconds}`;
+  }, 1000);
 }
 
 function stopButton(button) {
@@ -103,26 +105,32 @@ function stopButton(button) {
 
   button.addEventListener("click", () => {
     stopp(obj, id);
-    setInterval(() => {
-      stopp(obj, id);
-    }, 1);
   });
+}
+
+function pauseButton(button) {
+  const id = button.dataset.id;
+  const obj = timers[id - 1];
+
+  button.addEventListener("click", () => {
+    pause(obj, id);
+  });
+}
+
+function pause(obj, id) {
+  obj.stopCounting = true;
 }
 
 function stopp(obj, id) {
   obj.stopCounting = true;
 
   obj.m = 0;
-  obj.ms = 0;
   obj.s = 0;
 
   const minutes = obj.m < 10 ? `0${obj.m}` : obj.m;
   const seconds = obj.s < 10 ? `0${obj.s}` : obj.s;
-  const ms = obj.ms < 10 ? `0${obj.ms}` : obj.ms;
 
   timerList[
     id - 1
-  ].children[0].innerHTML = `<span>${id}. ${minutes}:${seconds}:${ms}</span>`;
+  ].children[0].innerHTML = `<span>${id}. ${minutes}:${seconds}</span>`;
 }
-
-// START BI LOI
